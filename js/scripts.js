bf2162c9241ef61d1dc68013c86c389d5bb344b9
2014maximo <alexuriel.777@gmail.com>
2019-08-19 22:27:15 -0500

    Se agrega la vista recepcionista

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 function administrador() {
-    window.open("vistas/administrador.html");
+
+    var usuario = document.getElementById('usuario').value;
+    var password = document.getElementById('password').value;
+
+    if((nombre == 'ADMINISTRADOR') && (password == 'asd123')){
+        window.open("vistas/administrador.html");
+    }else if ((nombre == 'RECEPCIONISTA') && (password == 'asd123')){
+        window.open("vistas/recepcionista.html");
+    }
 }
-function formulario() {
-    window.open("vistas/administrador.html");
-}
+
+
