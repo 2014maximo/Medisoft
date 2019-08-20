956757e0af634c14f16524e32ea8def3a31ad183
2014maximo <alexuriel.777@gmail.com>
2019-08-19 22:48:40 -0500

    Reparación del formulario

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ function administrador() {
     var usuario = document.getElementById('usuario').value;
     var password = document.getElementById('password').value;
 
-    if((nombre == 'ADMINISTRADOR') && (password == 'asd123')){
+    if((usuario == 'ADMINISTRADOR') && (password == 'asd123')){
         window.open("vistas/administrador.html");
-    }else if ((nombre == 'RECEPCIONISTA') && (password == 'asd123')){
+    }else if ((usuario == 'RECEPCIONISTA') && (password == 'asd123')){
         window.open("vistas/recepcionista.html");
     }
 }
