e776c5c2390ae1db5de2b36fee9f443f65f2259c
2014maximo <alexuriel.777@gmail.com>
2019-08-20 22:15:37 -0500

    Agregado agenda y servicios

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,10 @@ function administrador() {
     var usuario = document.getElementById('usuario').value;
     var password = document.getElementById('password').value;
 
-<<<<<<< HEAD
-    if((nombre == 'ADMINISTRADOR') && (password == 'asd123')){
-        window.open("vistas/administrador.html");
-    }else if ((nombre == 'RECEPCIONISTA') && (password == 'asd123')){
-=======
     if((usuario == 'ADMINISTRADOR') && (password == 'asd123')){
         window.open("vistas/administrador.html");
     }else if ((usuario == 'RECEPCIONISTA') && (password == 'asd123')){
->>>>>>> gh-pages
         window.open("vistas/recepcionista.html");
     }
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> gh-pages
